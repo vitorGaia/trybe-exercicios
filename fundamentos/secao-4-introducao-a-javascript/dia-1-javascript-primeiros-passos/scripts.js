d839faca7dd97c5e00a49134ff04b808c6aa8fdf
vitorGaia <vitorgaia0027@gmail.com>
2022-11-23 18:53:27 -0300

    estou no if/else 4 agora

--- fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,6 +1,57 @@
-//Usar Ctrol + S para salvar o código sempre antes de executar
+// Usar Ctrol + S para salvar o código sempre antes de executar
+// Operadores aritméticos
 
-let myName = "Martins";
+/*const a = 3;
+const b = 2;
 
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
 
-console.log(myName);
+// if/else
+if (a > b) {
+    console.log('a maior que b');
+} else {
+    console.log('b maior que a');
+}
+
+// if/else 2.0
+const x = 10;
+const y = 50;
+const z = 20;
+
+if (x > y && x > z) {
+    console.log('x maior que y e z');
+} else if (y > x && y > z) {
+    console.log('y maior que x e z');
+} else if (z > x && z > y) {
+    console.log('z maior que x e y');
+}
+
+// if/else 3.0
+let numero = -15;
+
+if (numero > 0) {
+    console.log('numero positivo');
+} else if (numero < 0) {
+    console.log('numero negativo');
+} else if (numero = 0) {
+    console.log('zero');
+}*/
+
+// if/else 4.0
+const a = 100;
+const b = 40;
+const c = -40.5;
+let eTriangulo = a + b + c;
+console.log(eTriangulo);
+
+if (eTriangulo == 180) {
+    console.log('true');
+} else if (eTriangulo !== 180) {
+    console.log('false');
+} else {
+    console.log('erro');
+}
