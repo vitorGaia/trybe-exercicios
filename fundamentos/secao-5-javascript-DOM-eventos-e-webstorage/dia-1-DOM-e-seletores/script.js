57a682d30837e4f620ede4404ac1787d2c6fb9be
vitorGaia <vitorgaia0027@gmail.com>
2022-12-07 18:04:05 -0300

    primeiros exercícios

---
+++ fundamentos/secao-5-javascript-DOM-eventos-e-webstorage/dia-1-DOM-e-seletores/script.js
@@ -0,0 +1,10 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const paragraphs = document.getElementsByClassName('paragraphs');
+paragraphs[0].style.color = 'green';
+paragraphs[1].style.fontSize = '50px';s
+
+const subtitle = document.getElementsByTagName('h4')[0];
+subtitle.style.textDecoration = 'none';
+subtitle.style.color = 'red';
