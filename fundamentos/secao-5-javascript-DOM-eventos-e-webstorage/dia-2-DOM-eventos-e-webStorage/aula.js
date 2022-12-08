9babb3d63926021a7eae725c066e38134385f231
vitorGaia <vitorgaia0027@gmail.com>
2022-12-08 19:30:57 -0300

    alguns dos exercícios do dia

--- fundamentos/secao-5-javascript-DOM-eventos-e-webstorage/dia-2-DOM-eventos-e-webStorage/aula.js
+++ fundamentos/secao-5-javascript-DOM-eventos-e-webstorage/dia-2-DOM-eventos-e-webStorage/aula.js
@@ -1,3 +1,4 @@
+/*
 //Acesse o elemento elementoOndeVoceEsta.
 const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 
@@ -23,3 +24,40 @@ const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
 
 //Agora acesse o terceiroFilho a partir de pai.
 const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+*/
+
+const pai = document.getElementById('pai');
+const irmaoElementoOndeVoceEsta = document.createElement('section');
+irmaoElementoOndeVoceEsta.id = 'irmãoElementoOndeVoceEsta';
+pai.appendChild(irmaoElementoOndeVoceEsta);
+
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+const filhoElementoOndeVoceEsta = document.createElement('p');
+filhoElementoOndeVoceEsta.innerText = 'Eu sou filho do elementoOndeVoceEsta!';
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+// isso não funcionou aqui mas funcionou acima
+const primeiroFilhoDoFilho = document.getElementById('PrimeiroFilhoDoFilho');
+const filhoDoPrimeiroFilhoDoFilho = document.createElement('p');
+filhoDoPrimeiroFilhoDoFilho.innerHTML = 'Eu sou filho do primeiroFilhoDoFilho!';
+primeiroFilhoDoFilho.appendChild(filhoDoPrimeiroFilhoDoFilho);
+
+
+const terceiroFilho = filhoDoPrimeiroFilhoDoFilho
+  .parentElement // primeiroFilhoDoFilho
+  .parentElement // elementoOndeVoceEsta
+  .nextElementSibling; // terceiroFilho
+console.log(terceiroFilho);
+
+
+// isso tbm não funcionou
+const paiDoPai = document.getElementById('paiDoPai');
+const todosOsFilhos = paiDoPai.childNodes;
+
+for (let index = todosOsFilhos.length - 1; index >= 0; index -= 1) {
+  const filhoAtual = todosOsFilhos[index];
+
+  if (filhoAtual.id == 'pai') {
+    filhoAtual.removeChild(filhoAtual.firstElementChild)
+  }
+}
