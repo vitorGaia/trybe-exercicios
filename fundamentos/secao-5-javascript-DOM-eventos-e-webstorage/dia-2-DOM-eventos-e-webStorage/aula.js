cbe4865a74b80ba991dd004a644191dc9fdcc16e
vitorGaia <vitorgaia0027@gmail.com>
2022-12-08 13:29:50 -0300

    parte 1 - buscando elementos

---
+++ fundamentos/secao-5-javascript-DOM-eventos-e-webstorage/dia-2-DOM-eventos-e-webStorage/aula.js
@@ -0,0 +1,25 @@
+//Acesse o elemento elementoOndeVoceEsta.
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+
+//Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+const pai = elementoOndeVoceEsta.parentElement;
+pai.style.color = 'red';
+
+//Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
+const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
+primeiroFilhoDoFilho.innerHTML = 'Primeiro filho do filho';
+
+//Acesse o primeiroFilho a partir de pai.
+const primeiroFilho = pai.firstElementChild;
+
+//Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+const primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
+
+//Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+const textElement = elementoOndeVoceEsta.nextSibling;
+
+//Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+
+//Agora acesse o terceiroFilho a partir de pai.
+const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
