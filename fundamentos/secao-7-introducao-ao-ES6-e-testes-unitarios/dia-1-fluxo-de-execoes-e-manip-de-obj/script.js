51834b965a5654d54eba8f8e37b4d2d91c2814fa
vitorGaia <vitorgaia0027@gmail.com>
2023-01-19 15:15:21 -0300

    instala jest

---
+++ fundamentos/secao-7-introducao-ao-ES6-e-testes-unitarios/dia-1-fluxo-de-execoes-e-manip-de-obj/script.js
@@ -0,0 +1,29 @@
+
+/*// Adicione suas informações ao objeto pessoaEstudante = {}, como nome, email, telefone, github e linkedIn, usando os métodos aprendidos neste conteúdo.
+const pessoaEstudante = {};
+
+const adicionaPropriedade = (objeto, novaPropriedade, valor) => {
+  objeto[novaPropriedade] = valor
+};
+
+adicionaPropriedade(pessoaEstudante, `nome`, `Fulano da Silva`);
+adicionaPropriedade(pessoaEstudante, `email`, `exemplo-fulano.silva@email.com`);
+adicionaPropriedade(pessoaEstudante, `telefone`, `11 1111-1111`);
+adicionaPropriedade(pessoaEstudante, `github`, `github.com/exemplo-fulano-da-silva`);
+adicionaPropriedade(pessoaEstudante, `linkedIn`, `linkedin.com/exemplo-fulano-da-silva`);*/
+
+/* // Object.entries() exemplo
+const coutries = {
+  franca: 'Paris',
+  brasil: 'Brasília',
+  espanha: 'Madrid',
+  portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(coutries);
+console.log(pairKeyValue);
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};*/
