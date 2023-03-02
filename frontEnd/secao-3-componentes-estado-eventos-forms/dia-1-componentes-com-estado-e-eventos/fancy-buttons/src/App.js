e2ec399cdf731ebf17088468ad08654f73ae26ad
vitorGaia <vitorgaia0027@gmail.com>
2023-03-02 15:37:56 -0300

    fnaliza exercicios de fixação

--- frontEnd/secao-3-componentes-estado-eventos-forms/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ frontEnd/secao-3-componentes-estado-eventos-forms/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,26 +1,50 @@
 import './App.css';
 import React from 'react';
 
-const textBtn1 = () => console.log('lau'); // não é boa prática funções fora do escopo da classe
-
 class App extends React.Component {
   constructor() {
     super();
+    /* this.state = {
+      clickNumber1: 0,
+      clickNumber2: 0,
+      clickNumber3: 0,
+    } */
     this.textBtn2 = this.textBtn2.bind(this);
   }
 
-  textBtn2 () {
-    console.log(this)
+  state = {
+    clickNumber1: 0,
+    clickNumber2: 0,
+    clickNumber3: 0,
+  } // esta sintaxe parece mais simples, combinada com arrow function não é necessário usar o .bind() nem constructor
+
+  textBtn1 = () => {
+    this.setState({
+      clickNumber1: 27,
+    })
   };
 
-  textBtn3 = () => console.log(this); // com a sintaxe de arrow conseguimos ver o this sem a necessidade de usar o constructor como no textBtn2 por exemplo.
+  textBtn2 () {
+    this.setState((previousState, _props) => ({
+      clickNumber2: previousState.clickNumber2 + 1,
+    }))
+  };
+
+  textBtn3 = () => {
+    this.setState((previousState, _props) => ({
+      clickNumber3: previousState.clickNumber3 + 1,
+    }))
+  }; // com a sintaxe de arrow conseguimos ver o this sem a necessidade de usar o constructor como no textBtn2 por exemplo.
+
+  btnColor = (num) => num % 2 === 0 ? 'green' : 'red';
 
   render() {
+    const { clickNumber1, clickNumber2, clickNumber3 } = this.state;
     return(
       <>
-        <button onClick={textBtn1}>1</button>
-        <button onClick={this.textBtn2}>2</button>
-        <button onClick={this.textBtn3}>3</button>
+        <button onClick={this.textBtn1}>{clickNumber1}</button>
+        <button onClick={this.textBtn2} style={ { backgroundColor: this.btnColor(clickNumber2) } }>{clickNumber2}</button>
+        <button onClick={this.textBtn3} style={ { backgroundColor: this.btnColor(clickNumber3) } }>{clickNumber3}</button>
       </>
     );
   }
