5698ded523b83f4ebbe5df84c91aaa550c012df5
vitorGaia <vitorgaia0027@gmail.com>
2023-04-04 15:25:04 -0300

    finaliza course

---
+++ frontEnd/secao-7-redux/dia-2-usando-redux-no-react/my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from 'react-redux';
+import { actionCreator } from '../src/redux/actions/index';
+
+class App extends React.Component {
+  render () {
+    const { dispatch, countState } = this.props;
+    return (
+      <div>
+        <h1>Contador</h1>
+        <h2>{ countState }</h2>
+        <button onClick={() => dispatch(actionCreator())}>Incrementa 1</button>
+        <button onClick={() => dispatch(actionCreator(5))}>Incrementa 5</button>
+      </div>
+    );
+  }
+}
+
+// o connect passa o estado global pra essa função
+const mapStateToProps = (state) => ({ countState: state.counterReducer.count });
+
+export default connect(mapStateToProps)(App);
---
+++ frontEnd/secao-7-redux/dia-2-usando-redux-no-react/my-app/src/reducers/counterReducer.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+
+const INITIAL_STATE = { count: 0, clicks: 0 };
+
+const counterReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'INCREMENT_COUNTER':
+      return {
+        ...state,
+        count: state.count + action.payload,
+      }
+    case 'INCREMENT_CLICK':
+      return {
+        ...state,
+        clicks: state.clicks + 1,
+      };
+    default:
+      return state
+  }
+}
+
+export const rootReducer = combineReducers({ counterReducer });
