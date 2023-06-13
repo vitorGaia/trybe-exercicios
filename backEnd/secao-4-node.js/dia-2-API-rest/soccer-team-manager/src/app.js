bcf8a463c38f0b19bd2891f2970a6f095d7e0362
vitorGaia <vitorgaia0027@gmail.com>
2023-06-13 19:02:42 -0300

    finaliza corse

---
+++ backEnd/secao-4-node.js/dia-2-API-rest/soccer-team-manager/src/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
+// ler
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá mundo!' }));
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+// criar
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam }); // 201 quer dizer created
+});
+
+// atualizar
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+
+  const updateTeam = teams.find((team) => team.id === +id);
+
+  if (!updateTeam) {
+    res.status(404).json({ message: 'Team not found' });
+  }
+
+  updateTeam.name = name;
+  updateTeam.initials = initials;
+  res.status(200).json({ updateTeam });
+});
+
+// deletar
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200).end();
+});
+
+module.exports = app;
