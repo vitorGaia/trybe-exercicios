65432553679210a78f25f8f2c4b01de262cb7238
vitorGaia <vitorgaia0027@gmail.com>
2023-07-11 09:40:26 -0300

    adiciona exemplo de transação gerenciada e não  gerenciada.

--- backEnd/secao-6-ORM-Sequelize/dia-2-ORM-associations-1pra1-1praN/associations-sequelize/src/services/employee.service.js
+++ backEnd/secao-6-ORM-Sequelize/dia-2-ORM-associations-1pra1-1praN/associations-sequelize/src/services/employee.service.js
@@ -1,4 +1,10 @@
 const { Address, Employee } = require('../models/');
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+
+const env = process.env.NODE_ENV || 'development';
+
+const sequelize = new Sequelize(config[env]);
 
 const getAll = async () => {
   const users = await Employee.findAll({
@@ -16,9 +22,50 @@ const getById = async (id) => {
       }],
     });
   return employee;
-}
+};
+
+// transação não gerenciada
+/* const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  try {
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
+
+    await t.commit();
+    return employee;
+  } catch (e) {
+    await t.rollback();
+    console.log(e);
+    throw e;
+  }
+}; */
+
+// transação gerenciada
+const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  const result = await sequelize.transaction(async (t) => {
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
+
+    return employee;
+  });
+
+  return result;
+};  
 
 module.exports = {
   getAll,
   getById,
+  insert,
 };
