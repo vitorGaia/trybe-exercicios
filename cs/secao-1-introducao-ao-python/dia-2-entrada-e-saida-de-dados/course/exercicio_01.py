4555816179d0e7d79a21d9c2b6aeda9970bdb03a
vitorGaia <vitorgaia0027@gmail.com>
2023-09-13 18:45:13 -0300

    adiciona arquivos de stup para os exercicios

---
+++ cs/secao-1-introducao-ao-python/dia-2-entrada-e-saida-de-dados/course/exercicio_01.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que solicite o nome de uma pessoa usuária e imprima-o na
+vertical.
+'''
+
+name = input('Digite seu nome! ')
+
+for el in name:
+    print(el.upper())
---
+++ cs/secao-1-introducao-ao-python/dia-2-entrada-e-saida-de-dados/course/exercicio_02.py
@@ -0,0 +1,9 @@
+'''
+Escreva um programa que receba vários números naturais e calcule a soma desses
+valores. Caso algum valor da entrada seja inválido (por exemplo uma letra), uma
+mensagem deve ser exibida na saída de erros no seguinte formato: “Erro ao
+somar valores, {valor} é um valor inválido”. Ao final, você deve imprimir a
+soma dos valores válidos.
+'''
+
+valores = input('Digite os valores a serem somados, separados por " "! ')
