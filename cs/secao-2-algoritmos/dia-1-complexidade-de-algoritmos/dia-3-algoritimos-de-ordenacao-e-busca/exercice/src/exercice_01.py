fafc3bfb54e87186204645684022176ff8663aaf
vitorGaia <vitorgaia0027@gmail.com>
2023-09-20 14:04:46 -0300

    finaliza exercicio 3.

---
+++ cs/secao-2-algoritmos/dia-1-complexidade-de-algoritmos/dia-3-algoritimos-de-ordenacao-e-busca/exercice/src/exercice_01.py
@@ -0,0 +1,9 @@
+'''
+Dado um array com os seguintes elementos ["zebra", "macaco", "elefante",
+"arara", "javali"], após duas iterações utilizando BUBBLLE SORT, como estaria
+este array?
+'''
+
+'''
+["elefante", "arara", "javali" ,"macaco","zebra"]
+'''
