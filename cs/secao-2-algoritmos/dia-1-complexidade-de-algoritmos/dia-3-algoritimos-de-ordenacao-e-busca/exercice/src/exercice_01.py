ee4ea1d5fda745afdd0ebd27dfbf465905872a7c
vitorGaia <vitorgaia0027@gmail.com>
2023-09-22 21:11:10 -0300

    Revert "finaliza exercicio 3."
    This reverts commit fafc3bfb54e87186204645684022176ff8663aaf.

--- cs/secao-2-algoritmos/dia-1-complexidade-de-algoritmos/dia-3-algoritimos-de-ordenacao-e-busca/exercice/src/exercice_01.py
+++
@@ -1,9 +0,0 @@
-'''
-Dado um array com os seguintes elementos ["zebra", "macaco", "elefante",
-"arara", "javali"], após duas iterações utilizando BUBBLLE SORT, como estaria
-este array?
-'''
-
-'''
-["elefante", "arara", "javali" ,"macaco","zebra"]
-'''
