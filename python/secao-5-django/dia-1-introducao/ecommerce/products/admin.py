ad3e81685722d61cce3463151d5213be9683234f
vitorGaia <vitorgaia0027@gmail.com>
2023-11-23 17:16:26 -0300

    config: adiciona configurações par uso do painel de adm.

--- python/secao-5-django/dia-1-introducao/ecommerce/products/admin.py
+++ python/secao-5-django/dia-1-introducao/ecommerce/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from products.models import Product
+from products.models import Customer
 
-# Register your models here.
+
+admin.site.site_header = 'Trybe Products E-commerce'
+admin.site.register(Product)
+admin.site.register(Customer)
--- python/secao-5-django/dia-1-introducao/ecommerce/products/models.py
+++ python/secao-5-django/dia-1-introducao/ecommerce/products/models.py
@@ -12,8 +12,14 @@ class Product(models.Model):
         upload_to="media/products", null=False, blank=True,
     )
 
+    def __str__(self):
+        return f'{self.name} - {self.price}'
+
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.name}'
