8f81d8989d3ce2bbb1d17d7be667a3d88f2322b2
vitorGaia <vitorgaia0027@gmail.com>
2023-11-23 16:53:56 -0300

    feat: adiciona tabela de produtos.

---
+++ python/secao-5-django/dia-1-introducao/ecommerce/ecommerce/products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.IntegerField(default=0)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        upload_to="media/products", null=False, blank=True,
+    )
