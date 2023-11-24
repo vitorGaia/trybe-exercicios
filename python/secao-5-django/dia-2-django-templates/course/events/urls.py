cefb2f33ccc14107e87ab7b376c3d0ef2954807b
vitorGaia <vitorgaia0027@gmail.com>
2023-11-24 15:01:06 -0300

    feat: adiciona pagina about

--- python/secao-5-django/dia-2-django-templates/course/events/urls.py
+++ python/secao-5-django/dia-2-django-templates/course/events/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from events.views import index
+from events.views import index, about
 
 
 urlpatterns = [
-    path('', index, name='home-page')
+    path('', index, name='home-page'),
+    path('about', about, name='about-page')
 ]
--- python/secao-5-django/dia-2-django-templates/course/events/views.py
+++ python/secao-5-django/dia-2-django-templates/course/events/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
