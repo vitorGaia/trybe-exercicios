297e873d9e5a26bae6c0edb82925aa45cf722807
vitorGaia <vitorgaia0027@gmail.com>
2023-11-27 13:59:07 -0300

    config: configura db e adiciona as primeiras models.

---
+++ python/secao-5-django/dia-3-forms-e-relacionamentos-de-modelos/course/database/01_create_database.sql
@@ -0,0 +1,4 @@
+/* database/01_create_database.sql */
+CREATE DATABASE IF NOT EXISTS playlist_manager_database;
+
+USE playlist_manager_database;
---
+++ python/secao-5-django/dia-3-forms-e-relacionamentos-de-modelos/course/playlists/models.py
@@ -0,0 +1,29 @@
+# playlists/models.py
+
+from django.db import models
+
+
+class Singer(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Music(models.Model):
+    name = models.CharField(max_length=50)
+    recorded_at = models.DateField()
+    length_in_seconds = models.IntegerField()
+
+    def __str__(self):
+        return self.name
