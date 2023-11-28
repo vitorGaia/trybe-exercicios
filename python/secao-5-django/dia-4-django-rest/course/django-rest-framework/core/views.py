162468225418c633dadc601abb8bfbb9709b1d1e
vitorGaia <vitorgaia0027@gmail.com>
2023-11-28 15:52:43 -0300

    feat: adiciona  serializers.

--- python/secao-5-django/dia-4-django-rest/course/django-rest-framework/core/views.py
+++ python/secao-5-django/dia-4-django-rest/course/django-rest-framework/core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Singer, Music, Playlist
+from .serializers import SingerSerializer, MusicSerializer, PlaylistSerializer
 
-# Create your views here.
+
+class SingerViewSet(viewsets.ModelViewSet):
+    queryset = Singer.objects.all()
+    serializer_class = SingerSerializer
+
+
+class MusicViewSet(viewsets.ModelViewSet):
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
+
+
+class PlaylistViewSet(viewsets.ModelViewSet):
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistSerializer
--- python/secao-5-django/dia-4-django-rest/course/django-rest-framework/playlistify/urls.py
+++ python/secao-5-django/dia-4-django-rest/course/django-rest-framework/playlistify/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from core.views import SingerViewSet, MusicViewSet, PlaylistViewSet
+
+router = routers.DefaultRouter()
+router.register(r'singers', SingerViewSet)
+router.register(r'musics', MusicViewSet)
+router.register(r'playlists', PlaylistViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
