422253d3182e0a47d202173afb385bc09a419306
vitorGaia <vitorgaia0027@gmail.com>
2023-11-28 15:07:38 -0300

    feat: adiciona models da aula anterior.

--- python/secao-5-django/dia-4-django-rest/course/django-rest-framework/core/admin.py
+++ python/secao-5-django/dia-4-django-rest/course/django-rest-framework/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Singer, Music, Playlist
 
-# Register your models here.
+admin.site.register(Singer)
+admin.site.register(Music)
+admin.site.register(Playlist)
